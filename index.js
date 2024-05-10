1ac73cce4b39036b16cc073522dae8751e2a941a
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-05-10 20:48:45 +0100

    updated HTML and JS.

--- index.js
+++ index.js
@@ -7,21 +7,15 @@ const saveEl = document.getElementById("save-para");
 let count = 0;
 
 //Increments the counter.
-function counter() {
+incrementBtn.addEventListener("click", function () {
   count += 1;
   countEl.innerText = count;
-}
+});
 
 // Saves the numbers in the counter just below the buttons and sets the number back to 0.
-function save() {
+saveBtn.addEventListener("click", function () {
   const saveResults = " " + count + " - ";
   saveEl.textContent += saveResults;
   countEl.textContent = 0;
   count = 0;
-}
-
-// when the button is clicked the counter function above is executed.
-incrementBtn.addEventListener("click", counter);
-
-// when the save button is clicked the number is saved.
-saveBtn.addEventListener("click", save);
+});
